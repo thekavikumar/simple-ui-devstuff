3aaec842d701eb28e0fcd59771f715dec2e1e004
Kavikumar M <kavikumarceo@gmail.com>
2022-12-02 14:46:16 +0530

    button done v1.0.4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import Button from "./lib/components/Button";
+import Button from "./lib/components/Button/Button";
 // import { Button } from "simple-ui-devstuff";
 
 function App() {
   return (
     <div className="App">
       {/* <Button label="Kavi" /> */}
-      <Button label="Submit" size="text-sm" type="alert" />
+      {/* <Button label="Submit" size="text-sm" type="alert" /> */}
     </div>
   );
 }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
+import Card from "./components/Cards/Card";
 
-export {Button};
+export { Button, Card };
